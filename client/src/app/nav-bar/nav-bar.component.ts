5ba07b96694d2f312a9a3a18295dfffc32b0c071
Fabian Rojas <fabian.rojas02@alumnos.ucn.cl>
2023-02-09 02:40:22 -0300

    feat(Angular): :lipstick: se agrega navbar

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -8,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  toggle = 'light'
+
+  toggleTheme() {
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+          this.toggle = 'dark'
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+      } else {
+          this.toggle = 'light'
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+      }
+
+      // if NOT set via local storage previously
+  } else {
+      if (document.documentElement.classList.contains('dark')) {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+          this.toggle = 'light'
+      } else {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+          this.toggle = 'dark'
+      }
+  }
+  }
 }
